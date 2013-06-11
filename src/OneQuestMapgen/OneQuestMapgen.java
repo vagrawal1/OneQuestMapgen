3f80bbcb9f868296c1f3d234cc98a8c19eef9901
dreadiscool <dreadiscool@gmail.com>
2013-06-11 08:34:18 -0400

    New map saving technique

--- src/OneQuestMapgen/OneQuestMapgen.java
+++ src/OneQuestMapgen/OneQuestMapgen.java
@@ -9,13 +9,13 @@ import tiles.Tile;
 import tiles.TileSet;
 
 public class OneQuestMapgen extends JFrame {
-	
-	private static final long serialVersionUID = 1L;
-	public static OneQuestMapgen instance;
-	
-	ArrayList<ArrayList<Tile>> map = new ArrayList<ArrayList<Tile>>();
-	TileSet tileSet = new TileSet();
-	SaveButton save = new SaveButton(0,0,10,tileSet.get("grasslands"));
+    
+    private static final long serialVersionUID = 1L;
+    public static OneQuestMapgen instance;
+    
+    ArrayList<ArrayList<Tile>> map = new ArrayList<ArrayList<Tile>>();
+    TileSet tileSet = new TileSet();
+    SaveButton save = new SaveButton(0,0,10,tileSet.get("grasslands"));
 
     public static void main(String[] args) {
         instance = new OneQuestMapgen();
@@ -23,50 +23,49 @@ public class OneQuestMapgen extends JFrame {
         instance.start();
     }
 
-	public void init(){
-		this.setSize(950,600);
+    public void init(){
+        this.setSize(950,600);
         this.setVisible(true);
         this.setTitle("OneQuest Map Generator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	public void start(){
-		
-		MouseEventListener mel = new MouseEventListener(this);
-		KeyEventListener kel = new KeyEventListener(this);
-		
-		addMouseListener(mel);
-		addMouseMotionListener(mel);
-		addKeyListener(kel);
-		
-		int tileSize = tileSet.get("grasslands")[1].getHeight();
-		
-		for (int i = 0; i < getHeight(); i += tileSize) {
-			ArrayList<Tile> temp = new ArrayList<Tile>();
-			for (int j = 0; j < getWidth(); j += tileSize) {
-				temp.add(new Tile(j, i, tileSize, tileSet.get("grasslands")));
-			}
-			map.add(temp);
-		}
-	}
-	
-	public void paint(Graphics g){
-		
-		for (int i = 0; i < map.size(); i++){
-			for(int j = 0; j < map.get(i).size(); j++){
-				map.get(i).get(j).render(g);
-			}				
-		}
-		save.render(g);		
-	}
-	
-	public void stop(){
-	}
-	
-	public void destroy(){
-	}
-	
-	public ArrayList<ArrayList<Tile>> getMap(){
-		return map;
-	}
+    }
+    
+    public void start(){
+        
+        MouseEventListener mel = new MouseEventListener(this);
+        KeyEventListener kel = new KeyEventListener(this);
+        
+        addMouseListener(mel);
+        addMouseMotionListener(mel);
+        addKeyListener(kel);
+        
+        int tileSize = tileSet.get("grasslands")[1].getHeight();
+        
+        for (int i = 0; i < getHeight(); i += tileSize) {
+            ArrayList<Tile> temp = new ArrayList<Tile>();
+            for (int j = 0; j < getWidth(); j += tileSize) {
+                temp.add(new Tile(j, i, tileSize, tileSet.get("grasslands")));
+            }
+            map.add(temp);
+        }
+    }
+    
+    public void paint(Graphics g){
+        for (int i = 0; i < map.size(); i++){
+            for(int j = 0; j < map.get(i).size(); j++){
+                map.get(i).get(j).render(g);
+            }                
+        }
+        save.render(g);        
+    }
+    
+    public void stop(){
+    }
+    
+    public void destroy(){
+    }
+    
+    public ArrayList<ArrayList<Tile>> getMap(){
+        return map;
+    }
 }
